b1b7fa73e25ff05cc69cf24dfe7ab780253dede8
Mohsen <msnazi@gmail.com>
2011-10-13 19:45:52 -0700

    new markup

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,24 @@
-/*
-ui.js
-User Interface related scripts goes here
-TODO
-*/
+/* Project: CSS3 Patternz
+ * Description: Generate CSS3 Patterns and the necessary code.
+ * Authors: Mohsen Nabiloo-Azimi (@mohsen____), Andrew Henderson (@AndrewHenderson)
+ * Start Date: Oct 12, 2011
+ */
+ 
+ $(document).ready(function(){
+ 
+	// make variable height divs equal heights
+    $.fn.sameHeights = function() {
+        $(this).each(function(){
+            var tallest = 0;
+            $(this).children().each(function(i){
+                if (tallest < $(this).height()) { tallest = $(this).height(); }
+            });
+            $(this).children().css({'height': tallest});
+        });
+        return this;
+    };
+	
+    // make boxes same height
+    $(".sameHeights").sameHeights();
+ 
+ });
