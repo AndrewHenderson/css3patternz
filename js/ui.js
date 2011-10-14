e52f782b7c85ce7f977c7716ddfd0a18f426e1c0
Mohsen <msnazi@gmail.com>
2011-10-14 00:31:08 -0700

    resizable footer

--- js/ui.js
+++ js/ui.js
@@ -22,3 +22,18 @@
     $(".sameHeights").sameHeights();
  
  });
+ 
+
+// footer resizable
+$('footer .resize.handle').bind('mousedown', function(mde){
+	var currentHeight = $('footer').height();
+	$(window).bind('mousemove', function(mme){
+		if(mme.pageY < innerHeight - 100 && mme.pageY > 150){
+			$('footer').height(currentHeight + mde.pageY - mme.pageY);
+			$('#output').height(footer.height() - 40);
+	    }
+		$(window).bind('mouseup', function(){
+			$(window).unbind('mousemove');
+		})
+	})
+})
