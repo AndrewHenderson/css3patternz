479d265bee4bc9a7f8eebd829cd12cbaf42524b5
Andrew Henderson <andrew.m.henderson@gmail.com>
2011-10-15 08:47:02 -0700

    Removed deprecated code

--- js/ui.js
+++ js/ui.js
@@ -4,25 +4,6 @@
  * Start Date: Oct 12, 2011
  */
  
- $(document).ready(function(){
- 
-	// make variable height divs equal heights
-    $.fn.sameHeights = function() {
-        $(this).each(function(){
-            var tallest = 0;
-            $(this).children().each(function(i){
-                if (tallest < $(this).height()) { tallest = $(this).height(); }
-            });
-            $(this).children().css({'height': tallest});
-        });
-        return this;
-    };
-	
-    // make boxes same height
-    $(".sameHeights").sameHeights();
- 
- });
- 
 //setAsideHeight
 function setAsideHeight(){
 	$('aside').height(innerHeight - 60 - $('footer').height());
