657870522a6b1e5b18907abbe026679e1c6140b3
Mohsen <msnazi@gmail.com>
2011-10-14 22:38:19 -0700

    scrollable aside

--- js/ui.js
+++ js/ui.js
@@ -23,17 +23,34 @@
  
  });
  
+//setAsideHeight
+function setAsideHeight(){
+	$('aside').height(innerHeight - 60 - $('footer').height());
+}
+// document load
 
+ $(document).ready(function(){
+ 	setAsideHeight();
+ });
+ 
 // footer resizable
 $('footer .resize.handle').bind('mousedown', function(mde){
 	var currentHeight = $('footer').height();
 	$(window).bind('mousemove', function(mme){
 		if(mme.pageY < innerHeight - 100 && mme.pageY > 150){
 			$('footer').height(currentHeight + mde.pageY - mme.pageY);
-			$('#output').height(footer.height() - 40);
+			$('#output').height($('footer').height() - 40);
+			setAsideHeight();
 	    }
 		$(window).bind('mouseup', function(){
 			$(window).unbind('mousemove');
 		})
 	})
 });
+<<<<<<< HEAD
+=======
+
+window.onresize = function  () {
+  setAsideHeight();
+}
+>>>>>>> scrollable aside
