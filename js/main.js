0f98bbe2799abb0f98923c39f231b2745d1f1ecb
Mohsen <msnazi@gmail.com>
2011-10-16 17:48:02 -0700

    API

--- js/main.js
+++ js/main.js
@@ -1,5 +1,47 @@
 /*
 main.js
-main scripts goes here
-TODO
 */
+
+var patternz = {
+  layers : [],
+	activeLayerIndex : 0,
+	activeLayer : null,
+	output : '',
+	prefixes: {webkit: true, moz: true, ms: true, o: true, w3c: true},
+	addLayer : function(){
+		var layer = {
+			width: 100,
+			height: 100,
+			background: 'transparent',
+			angle: 0,
+			strips: []
+		};
+		patternz.layers.push(layer);
+	},
+	changeActiveLayer : function (layerIndex) {
+	  patternz.activeLayer = patternz.layers[layerIndex];
+	},
+	modifyLayer: {
+		addStrip: function(stripColor, startPoint, endPoint){
+			var strip = {};
+			strip.color = stripColor || "#000";
+			strip.start = startPoint || 0;
+			strip.end = endPoint || 10;
+			patternz.activeLayer.strips.push(strip);
+		},
+		removeStrip: function(stripIndex){
+
+		},
+		changeStripSize: function(stripIndex, start, end){},
+		changeStripColor: function(stripIndex, color){}
+	},
+	generate: function(){
+		
+	},
+	init : function () {
+	  if(patternz.layers.length == 0) patternz.addLayer();
+	  patternz.changeActiveLayer(0);
+	}
+};
+
+patternz.init();
