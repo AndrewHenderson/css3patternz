dbfabb9c8c8f3c4c813a7134898db5f2290f1a5e
Andrew Henderson <andrew.m.henderson@gmail.com>
2011-12-07 00:19:49 -0800

    UI Dev

--- js/ui.js
+++ js/ui.js
@@ -8,7 +8,6 @@
 (function(){
 	ui.init();
 	var minMax = $(".layer:eq(1)").removeClass("minimized").find(".minimize").addClass("open");
-	minMax.find(".arrow").text("∧")
 	minMax.find(".toggle-word").text("Minimize");
 	$('.colorPreview').jPicker({ window: { expandable: true } });
 	
